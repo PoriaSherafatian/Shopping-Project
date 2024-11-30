48ba75faddf80b8244807b6dc1ef8646663c57f9
PoriaSherafatian <poriasherafatian@gmail.com>
2024-11-30 10:30:51 +0330

    Only want to connect mine BackEnd resource to GitHub

---
+++ src/test/java/Poria/dev/ShoppingProject/ShoppingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Poria.dev.ShoppingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
